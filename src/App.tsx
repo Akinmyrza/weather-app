d2a681a81885a887e9ace976f41c323a56abd7f3
Akinmyrza Kanat uulu <akinmyrza@mancho.dev>
2022-03-27 01:48:43 +0600

    Added api_key and get successfully first request.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 /* Local dependencies */
+import Search from './components/Search';
 import './App.css';
 
 export default function App() {
-  return <h1>Welcome to new project</h1>;
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <Search title={'Enter a City'} />
+    </div>
+  );
 }
